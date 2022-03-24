293bf8f3dad6c96b1592ef2d03d7fa0b4b32b0e4
alexkozlovv <79681592+alexkozlovv@users.noreply.github.com>
2022-03-24 19:49:21 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,55 +6,78 @@
 #include "periph_conf.h"
 #include "board.h"
 #include "periph/gpio.h"
+#include "msg.h"
 
 #define FLASHING1                1000000
 #define FLASHING2                100000
 
-
-int first_press = 1;
 xtimer_ticks32_t button_down;
-xtimer_ticks32_t duration_button_down;
+xtimer_ticks32_t button_up;
+int first_press = 1;
 int duration_button_us = 0;
 int flashing = 0;
 int long_press_try = 1;
+static char regular_stack[THREAD_STACKSIZE_DEFAULT];
+static kernel_pid_t thread_flashing;
+
 
 void btn_handler(void *arg) {
-	(void) arg;
-	button_down = xtimer_now();
-	while(!gpio_read(GPIO_PIN(PORT_C, 13))) {
-		duration_button_down = xtimer_now();
-		}
-	duration_button_us = xtimer_usec_from_ticks(duration_button_down);
-	if ((duration_button_us - xtimer_usec_from_ticks(button_down)) < 500000) {
-		if (first_press){
-			flashing = FLASHING1;
-			first_press = 0;
+    (void) arg;
+    msg_t msg;
+    if (!gpio_read(GPIO_PIN(PORT_C, 13)))
+        msg.content.value = 2;
+    else 
+    	msg.content.value = 3;
+    msg_send(&msg, thread_flashing);
+    return;
+}
+
+void* flashing_thread(void* arg) {
+    (void) arg;
+    while (1) {
+        msg_t msg;
+        msg_receive(&msg);
+        if (msg.content.value == 2)
+		    button_down = xtimer_now();
+		if (msg.content.value == 3) {
+		    button_up = xtimer_now();
+		    duration_button_us = xtimer_usec_from_ticks(xtimer_diff(button_up, button_down));   
+		    if (duration_button_us < 500000) {
+		        if (first_press){
+			    flashing = FLASHING1;
+			    first_press = 0;
 			}
-                else {
-                    flashing = 0;
-                    gpio_clear(GPIO_PIN(PORT_A, 5));
-                    first_press = 1;
+                        else {
+                            flashing = 0;
+                            gpio_clear(GPIO_PIN(PORT_A, 5));
+                            first_press = 1;
+                        }
+                   }
+                    if (duration_button_us > 500000) {
+       	        if (first_press)
+       	        ;
+       	        else if (long_press_try) {
+       	            flashing = FLASHING2;
+       	            long_press_try = 0;
+       	        }
+       	        else {
+       	            flashing = FLASHING1;
+       	            long_press_try = 1;
+       	        }
                     }
-           }
-       if ((duration_button_us - xtimer_usec_from_ticks(button_down)) > 500000) {
-       	if (first_press)
-       	    ;
-       	else if (long_press_try) {
-       	    flashing = FLASHING2;
-       	    long_press_try = 0;
-       	    }
-       	else {
-       	    flashing = FLASHING1;
-       	    long_press_try = 1;
-       	    }
-       }
-       return;
- }
-       	        
+		}
+    }
+    return 0;
+}
+
 int main(void) {
 	gpio_init(GPIO_PIN(PORT_C, 13), GPIO_IN);
 	gpio_init(GPIO_PIN(PORT_A, 5), GPIO_OUT);
-	gpio_init_int(GPIO_PIN(PORT_C, 13), GPIO_IN, GPIO_FALLING, btn_handler, NULL);
+	gpio_init_int(GPIO_PIN(PORT_C, 13), GPIO_IN, GPIO_BOTH, btn_handler, NULL);
+	thread_flashing = thread_create(regular_stack, sizeof(regular_stack),
+                               THREAD_PRIORITY_MAIN-1, THREAD_CREATE_STACKTEST,
+                               flashing_thread, NULL, "flashing thread");
+
 	while(1){
 		if (flashing){
 		    gpio_toggle(GPIO_PIN(PORT_A, 5));
@@ -69,6 +92,5 @@ int main(void) {
 	
 	return 0;
 }
-
 
 
